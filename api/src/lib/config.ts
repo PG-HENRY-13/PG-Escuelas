ec5dfad5552c274d0d8c0a1631c0e9dcf2378300
thomasHeitFux <thomas41392@gmail.com>
2022-04-26 13:06:47 -0300

    [delete]lib/config.ts

--- api/src/lib/config.ts
+++ api/src/lib/config.ts
@@ -4,9 +4,9 @@ dotenv.config();
 
 const config = {
 	dbUser: process.env.DB_USER || 'postgres',
-	dbPassword: process.env.DB_PASSWORD || '1234',
+	dbPassword: process.env.DB_PASSWORD || 'password',
 	dbHost: process.env.DB_HOST || 'localhost',
-	dbName: process.env.DB_NAME || 'workshop',
+	dbName: process.env.DB_NAME || 'PgHenry',
 	dbPort: process.env.DB_PORT || '5432',
 	dev: process.env.NODE_ENV !== 'production',
 	port: process.env.API_PORT || '3001',
