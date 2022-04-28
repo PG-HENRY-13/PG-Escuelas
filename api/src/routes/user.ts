c53c629ef2909aa5b324c0a82ba8dfb7ab788ce9
Sebastian <sebaprieto1@hotmail.com>
2022-04-28 17:41:05 -0300

    [Update] Switched database force to true

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,100 +1,95 @@
-import { Response, Request, Router, NextFunction } from 'express';
-import { json } from 'stream/consumers';
-import { User } from '../models/User';
+import { Response, Request, Router, NextFunction } from "express";
+import { json } from "stream/consumers";
+import { User } from "../models/User";
 const router = Router();
 
-
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-	User.findAll()
-		.then((users) => {
-			return res.send(users);
-		})
-		.catch((error) => {
-			return res.status(404).send(error);
-		});
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+  User.findAll()
+    .then((users) => {
+      return res.send(users);
+    })
+    .catch((error) => {
+      return res.status(404).send(error);
+    });
 });
 
-
-router.get ('/:userID', (req: Request, res: Response, next: NextFunction) => {
-	const userID = req.params.userID;
-	User.findByPk(userID)
-	.then((user) => {
-		if (user) return res.status(202).send(user);
-		else res.status(404).send ('The user does not exist');
-	})
-	.catch ((err) => {
-		return res.status(err.code).send(err.message);
-	})
-})
-
-
-
-router.post('/', (req: Request, res: Response, next: NextFunction) => {
-	const user = req.body;
-	User.create(user)
-		.then((createdUser) => {
-			return res.send(createdUser);
-		})
-		.catch((error) => {
-			return res.status(404).send(error);
-		});
+router.get("/:userID", (req: Request, res: Response, next: NextFunction) => {
+  const userID = req.params.userID;
+  User.findByPk(userID)
+    .then((user) => {
+      if (user) return res.status(202).send(user);
+      else res.status(404).send("The user does not exist");
+    })
+    .catch((err) => {
+      return res.status(err.code).send(err.message);
+    });
 });
 
-router.delete('/', async (req: Request, res: Response, next: NextFunction) => {
-	const { userID } = req.body;
-	const removedUser = await User.findByPk(userID);
-	if (!removedUser) { res.status(404).send("User not found") }
-	if (removedUser) {
-		removedUser.destroy()
-			.then(() => {
-				return res.status(202).send("Removed correctly")
-			})
-			.catch((error: any) => {
-				return res.status(404).send(error);
-			});
-	}
+router.post("/", (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body;
+  User.create(user)
+    .then((createdUser) => {
+      return res.send(createdUser);
+    })
+    .catch((error) => {
+      return res.status(404).send(error);
+    });
 });
 
-
-
-
-router.post('/update', async (req: Request, res: Response, next: NextFunction) => {
-  	try {
-
-    	const userUpdate: User = req.body;
-		//ES NECESARIO RECIBIR LOS DATOS DESDE EL BODY
-		
-    	const existingUser = await User.findByPk(parseInt(userUpdate.cuil));
-		
-    	if (existingUser) {
-			await User.update({
-				cuil: userUpdate.cuil,
-				name: userUpdate.name,
-				lastName: userUpdate.lastName,
-				phoneNumber: userUpdate.phoneNumber,
-				emailAddress: userUpdate.emailAddress,
-				address: userUpdate.address,
-
-				//AQUI AGREGAR LOS CAMPOS QUE SE QUIERAN MODIFICAR
-
-			}, {
-				where: {
-					cuil: userUpdate.cuil,	
-				}
-			});
-
-      		return res.status(200).json(userUpdate);
-    	}
-		else{
-    		
-			res.status(400).send("User not found");
-		}
-
-    
-  	} catch (e) {
-    res.status(400).send("ERROR"+e);
+router.delete("/", async (req: Request, res: Response, next: NextFunction) => {
+  const { userID } = req.body;
+  const removedUser = await User.findByPk(userID);
+  if (!removedUser) {
+    res.status(404).send("User not found");
+  }
+  if (removedUser) {
+    removedUser
+      .destroy()
+      .then(() => {
+        return res.status(202).send("Removed correctly");
+      })
+      .catch((error: any) => {
+        return res.status(404).send(error);
+      });
   }
 });
 
+router.post(
+  "/update",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userUpdate: User = req.body;
+      //ES NECESARIO RECIBIR LOS DATOS DESDE EL BODY
+
+      const existingUser = await User.findByPk(parseInt(userUpdate.cuil));
+
+      if (existingUser) {
+        await User.update(
+          {
+            cuil: userUpdate.cuil,
+            name: userUpdate.name,
+            lastName: userUpdate.lastName,
+            phoneNumber: userUpdate.phoneNumber,
+            emailAddress: userUpdate.emailAddress,
+            address: userUpdate.address,
+
+            //AQUI AGREGAR LOS CAMPOS QUE SE QUIERAN MODIFICAR
+          },
+          {
+            where: {
+              cuil: userUpdate.cuil,
+            },
+          }
+        );
+
+        return res.status(200).json(userUpdate);
+      } else {
+        res.status(400).send("User not found");
+      }
+    } catch (e) {
+      res.status(400).send("ERROR" + e);
+    }
+  }
+);
 
 export default router;
