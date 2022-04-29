659338b421417fb0131ed8824d1ff4cb26459a08
Joaquin Nogueira <89049593+JoaquinNogueira@users.noreply.github.com>
2022-04-29 12:33:37 -0300

    Change role.ts

--- api/src/routes/role.ts
+++ api/src/routes/role.ts
@@ -1,20 +1,20 @@
-import { Response, Request, Router, NextFunction } from 'express';
-import { User } from '../models/User';
+import { Response, Request, Router, NextFunction } from "express";
+import { User } from "../models/User";
 const router = Router();
 
- router.get ('/', async (req: Request, res: Response, next: NextFunction) => {
-    const role : any = req.query.role; 
-    if(role) {
-        const usersFilter = await User.findAll({
-            where: {
-                role
-            }
-        });
-        res.send(usersFilter);
-    } else {
-        const users = await User.findAll();
-        res.send(users);
-    }
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  const role: any = req.body;
+  if (role) {
+    const usersFilter = await User.findAll({
+      where: {
+        role,
+      },
+    });
+    res.send(usersFilter);
+  } else {
+    const users = await User.findAll();
+    res.send(users);
+  }
 });
- 
+
 export default router;
