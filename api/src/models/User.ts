07960fe51e14b7743a8e9eddb4ba10091edd5ef2
moronifacundo <moronifacundo@gmail.com>
2022-04-30 02:08:51 -0300

    [MERGE] /models/User - merging, nothing changed but spaces

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -5,11 +5,8 @@ import {
   CreatedAt,
   UpdatedAt,
   PrimaryKey,
-<<<<<<< HEAD
   BelongsToMany,
-=======
   DataType,
->>>>>>> 6df985113509dd7c036d011c7a890e4b4449d345
 } from "sequelize-typescript";
 import { Job } from "./Job";
 import { UsersJobs } from "./UsersJobs";
@@ -28,7 +25,6 @@ enum GenderType {
 
 @Table
 export class User extends Model<User> {
-<<<<<<< HEAD
   @BelongsToMany(() => Job, () => UsersJobs)
   jobs!: Job[];
 
@@ -68,44 +64,6 @@ export class User extends Model<User> {
   @Column
   createdAt!: Date;
 
-=======
-  @PrimaryKey
-  @Column
-  cuil!: string;
-
-  @Column
-  name!: string;
-
-  @Column
-  lastName!: string;
-
-  @Column
-  password!: string;
-
-  @Column
-  phoneNumber!: string;
-
-  @Column
-  emailAddress!: string;
-
-  @Column
-  address!: string;
-
-  @Column
-  gender!: GenderType; // Un enumerador que definí arriba con 3 casos
-
-  @Column
-  role!: RoleType; // Un enumerador que definí arriba con 3 casos
-
-  @CreatedAt
-  @Column
-  seniorityDate!: Date; // Es lo que se llama fecha de escalafón
-
-  @CreatedAt
-  @Column
-  createdAt!: Date;
-
->>>>>>> 6df985113509dd7c036d011c7a890e4b4449d345
   @UpdatedAt
   @Column
   updatedAt!: Date;
