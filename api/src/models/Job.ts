5f3092eba88bd034f4a1bf0656f7f83aac502c86
moronifacundo <moronifacundo@gmail.com>
2022-04-30 02:08:15 -0300

    [MERGE] /models/jobs - merging, nothing changed but spaces

--- api/src/models/Job.ts
+++ api/src/models/Job.ts
@@ -6,10 +6,7 @@ import {
   UpdatedAt,
   PrimaryKey,
   DataType,
-<<<<<<< HEAD
   BelongsToMany,
-=======
->>>>>>> 6df985113509dd7c036d011c7a890e4b4449d345
 } from "sequelize-typescript";
 import { InferAttributes, InferCreationAttributes } from "sequelize/types";
 import { User } from "./User";
@@ -21,7 +18,6 @@ interface Job1 {
 }
 
 @Table
-<<<<<<< HEAD
 export class Job extends Model<Job1> {
   @BelongsToMany(() => User, () => UsersJobs)
   users?: User[];
@@ -40,21 +36,4 @@ export class Job extends Model<Job1> {
   @UpdatedAt
   @Column
   updatedAt?: Date;
-=======
-export class Job extends Model<Job> {
-  @PrimaryKey
-  @Column
-  id!: string;
-
-  @Column
-  name!: string;
-
-  @CreatedAt
-  @Column
-  createdAt!: Date;
-
-  @UpdatedAt
-  @Column
-  updatedAt!: Date;
->>>>>>> 6df985113509dd7c036d011c7a890e4b4449d345
 }
