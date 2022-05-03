b659d717dd8d2b986c9dc2a74135b4c7bfcbbcd7
Sebastian <sebaprieto1@hotmail.com>
2022-05-03 13:18:45 -0300

    [FIXED] UserInfo - Now the filter can serch by Name and Surname

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -71,8 +71,15 @@ export default function UserInfo(): JSX.Element {
           {loadedUsers
             .filter((user: User) => {
               if (!filter) return true;
-              const name = user.name.toLowerCase();
-              return name.includes(filter.toLocaleLowerCase());
+              const Fullname =
+                user.name.toLowerCase() + " " + user.lastName.toLowerCase();
+              const ReverseFullname =
+                user.lastName.toLowerCase() + " " + user.name.toLowerCase();
+              return Fullname.includes(filter.toLocaleLowerCase())
+                ? user
+                : ReverseFullname.includes(filter.toLocaleLowerCase())
+                ? user
+                : false;
             })
             .map((e: any) => {
               return (
