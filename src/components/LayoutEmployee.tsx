603e0adf33013cee5d838fd7973e8dfca6b0499e
Sebastian <sebaprieto1@hotmail.com>
2022-05-09 12:33:14 -0300

    [Update] Personalized UserLayout

--- src/components/LayoutEmployee.tsx
+++ src/components/LayoutEmployee.tsx
@@ -1,37 +1,47 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
-import "../styles/Layouts.css";
+import "../styles/LayoutUser.css";
 export default function LayoutEmployee(): JSX.Element {
   return (
     <main>
-      <nav className="navbar navbar-expand-lg">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/createuser">
-              Mi perfil
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/user/news">
-              Novedades
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <span className="nav-link">Contingencias</span>
-            <ul>
-              <li>
-                <NavLink className="nav-link" to="/user/absenceReport">
-                  Ausencia
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to="/user/reschedule">
-                  Cambio de horario
-                </NavLink>
-              </li>
-            </ul>
-          </li>
-        </ul>
+      <nav className="menu">
+        <section className="menu__container">
+          <ul className="menu__links">
+            <li className="menu__item">
+              <NavLink className="menu__link" to="/admin/createuser">
+                Mi perfil
+              </NavLink>
+            </li>
+            <li className="menu__item">
+              <NavLink className="menu__link" to="/user/news">
+                Novedades
+              </NavLink>
+            </li>
+            <li className="menu__item menu__item--show">
+              <a href="#" className="menu__link">
+                Contingencias
+              </a>
+              <ul className="menu_nesting">
+                <li className="menu_inside">
+                  <NavLink
+                    className="menu__link menu__link--inside"
+                    to="/user/absenceReport"
+                  >
+                    Ausencia
+                  </NavLink>
+                </li>
+                <li className="menu_inside">
+                  <NavLink
+                    className="menu__link menu__link--inside"
+                    to="/user/reschedule"
+                  >
+                    Cambio de horario
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </section>
       </nav>
       <section>
         <Outlet />
