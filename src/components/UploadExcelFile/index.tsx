608f6de6eb1c5c0ae5cbffc5b6c9d1f8835833bc
moronifacundo <moronifacundo@gmail.com>
2022-05-09 01:30:40 -0300

    [CHANGE] component/UploadExcelFile - unused variable

--- src/components/UploadExcelFile/index.tsx
+++ src/components/UploadExcelFile/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { saveUsersFromExcelFile } from "../../redux/actions";
+// import { saveUsersFromExcelFile } from "../../redux/actions";
 import { useState, useEffect } from "react";
 
 export default function UploadExcelFile(): JSX.Element {
