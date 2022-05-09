933f3ee0a56164571941fc1614fbc7f38ff98440
moronifacundo <moronifacundo@gmail.com>
2022-05-09 01:14:32 -0300

    [CREATE] src/components/LoadUsersWithExcel - component with button to load the excel in the resources folder to the database

---
+++ src/components/LoadUsersWithExcel/index.tsx
@@ -0,0 +1,24 @@
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { saveUsersFromExcelFile } from "../../redux/actions";
+
+export default function LoadUsersWithExcel(): JSX.Element {
+  const dispatch = useDispatch();
+
+  function loadData(e: React.SyntheticEvent) {
+    e.preventDefault();
+    dispatch(saveUsersFromExcelFile() as any);
+  }
+
+  return (
+    <div className="login-container">
+      <div className="form-button-container">
+        <form onSubmit={loadData}>
+          <button className="button" type="submit">
+            Cargar Usuarios
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
