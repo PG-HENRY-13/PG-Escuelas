3415f57f1f661a5e7ed65050b5c5be39c18b9327
David <david@Davids-MacBook-Pro.local>
2022-05-13 03:01:42 -0300

    [CREATE] route forgot pwd one time token

--- api/src/routes/login.ts
+++ api/src/routes/login.ts
@@ -4,6 +4,11 @@ import { User } from "../models/User";
 import bcrypt from "bcrypt";
 import Joi from "joi";
 import { genToken } from "../utils/authToken";
+import jwt from "jsonwebtoken";
+import config from "../lib/config";
+
+const url = config.http + config.host + (config.frontPort?":"+config.frontPort:'');
+
 const router = Router();
 
 router.post("/", async (req: Request, res: Response, next: NextFunction) => {
@@ -14,15 +19,48 @@ router.post("/", async (req: Request, res: Response, next: NextFunction) => {
   });
   const { error } = schema.validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  
+
   let user = await User.findByPk(cuil);
   if (!user) return res.status(400).send("Usuario incorrecto");
 
   const isValid = await bcrypt.compare(password, user.password);
-  if (!isValid) return res.status(400).send({msg:'Contraseña incorrecta'});
+  if (!isValid) return res.status(400).send({ msg: "Contraseña incorrecta" });
 
   const token = genToken(user);
   res.send(token);
 });
 
+router.post("/forgotpwd", async (req, res) => {
+  if (req.body.cuil !== undefined) {
+    const cuil = req.body.cuil;
+
+    const user = await User.findByPk(cuil);
+
+    if (user) {
+      var payload = {
+        id: user.cuil,
+        email: user.emailAddress,
+      };
+      // se crea un one time token con el pwd hasheado + la fecha creado
+      var secret = user?.password + "-" + user.createdAt.getTime;
+
+      var token = jwt.sign(payload, secret);
+
+      // TODO: Send email containing link to reset password.
+      // In our case, will just return a link to click.
+      res.send(
+        `<a href="${url}/resetpassword/${payload.id}/${token}">Reset password</a>`
+      );
+    } else {
+      res.status(404).send("El cuil no existe");
+    }
+  } else {
+    res.send("Se ha perdido el cuil");
+  }
+});
+
+
+
+
 export default router;
+
